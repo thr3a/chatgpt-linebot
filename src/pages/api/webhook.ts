6bc1e599b35565728ce750b2ee923651d8bc7104
thr3a <thr3a@example.com>
2023-03-12 01:15:15 +0900

    add:

--- src/pages/api/webhook.ts
+++ src/pages/api/webhook.ts
@@ -1,25 +1,49 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import { Client, middleware } from "@line/bot-sdk";
+import { NextApiRequest, NextApiResponse } from 'next';
+import { Client, middleware, TextMessage, WebhookEvent, MessageAPIResponseBase } from '@line/bot-sdk';
+import {Configuration, OpenAIApi} from 'openai';
 
 const config = {
   channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN || 'xxx',
   channelSecret: process.env.LINE_CHANNEL_SECRET || 'xxx',
 };
-
 const client = new Client(config);
+
+const openAiConfig = new Configuration({
+  apiKey: process.env.OPENAI_APIKEY || 'xxx'
+});
+const openai = new OpenAIApi(openAiConfig);
+
+const fetchChatGPT = async (message: string): Promise<string> => {
+  const completion = await openai.createChatCompletion({
+    model: 'gpt-3.5-turbo',
+    messages: [{ role: 'user', content: message }],
+  });
+  const text = completion.data.choices[0].message?.content as string;
+  return text;
+};
+
+const handleMessage = async (event: WebhookEvent): Promise<MessageAPIResponseBase | undefined> => {
+  if (event.type !== 'message' || event.message.type !== 'text') {
+    return;
+  }
+  const { text } = event.message;
+  const replyText = await fetchChatGPT(text);
+  const replyObject: TextMessage = {
+    type: 'text',
+    text: replyText
+  };
+  await client.replyMessage(event.replyToken, replyObject);
+};
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.method === "POST") {
+  if (req.method === 'POST') {
     middleware(config)(req, res, async () => {
       const events = req.body.events;
       for (const event of events) {
-        if (event.type === "message" && event.message.type === "text") {
-          // オウム返しする
-          await client.replyMessage(event.replyToken, event.message);
-        }
+        handleMessage(event);
       }
     });
   } else {
