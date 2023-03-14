4599a2565ba27501d0033098e6272390ac25d084
thr3a <thr3a@example.com>
2023-03-15 02:49:17 +0900

    add: firebase

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,7 +5,9 @@ import { Header } from '../features/common/components/Header';
 import { Container } from '@mantine/core';
 import { PageProvider } from '../features/common/contexts/PageContext';
 import { Notifications } from '@mantine/notifications';
+import { initializeFirebaseApp } from '@/lib/firebase/firebase';
 
+initializeFirebaseApp();
 export default function App(props: AppProps) {
   const { Component, pageProps } = props;
 
